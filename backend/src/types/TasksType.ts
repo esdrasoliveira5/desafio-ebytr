8bd7bdd5ff94cbb25783429773983d66ad2b2683
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-19 17:21:04 -0300

    feat: implemented TasksType.ts

---
+++ backend/src/types/TasksType.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'; 
+
+const TasksSchema = z.object({
+  name: z.string({
+    required_error: 'name is required',
+    invalid_type_error: 'name must be a string',
+  }).min(3, { message: 'name must be 3 or more characters long' }),
+  description: z.string({
+    required_error: 'description is required',
+    invalid_type_error: 'description must be a string',
+  }).min(5, { message: 'description must be 5 or more characters long' }),
+  status: z.string().and(z.literal('pendente')
+    .or(z.literal('em andamento'))
+    .or(z.literal('pronto'))),
+  date: z.string().optional(),
+});
+
+export type Transaction = z.infer<typeof TasksSchema>;
+export { TasksSchema };
