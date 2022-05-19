0385c209868c397289820ecbe9cdbe0fb630f658
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-19 17:47:48 -0300

    test: implementesd UserModel.ts unit test

---
+++ backend/src/tests/unit/models/UserModel.test.ts
@@ -0,0 +1,39 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import UserModel from '../../../models/UserModel';
+
+const user = new UserModel();
+const { expect } = chai;
+
+const payload = {
+  _id: '6260bca97c58e5a0b7847cfa',
+  userame: 'Roberto',
+  email: 'roberto@email.com',
+  password: '$2b$10$JOmGDGptDGC1.eLa3OMj0uAk4FxZT2SjLH0lbP3Uh9W7iDHGN3Lp6',
+  tasks: [],
+}
+
+
+describe('2 - Test UserModel', () => {
+  describe('2.1 - method create', () => {
+    before(async () => {
+      sinon
+        .stub(user.model, 'create')
+        .resolves(payload);
+    });
+  
+    after(()=>{
+      sinon.restore();
+    })
+  
+    it('return the user created in the db', async () => {
+      const response = await user.create({
+        userName: 'Roberto',
+        email: 'roberto@email.com',
+        password: 'password',
+        tasks: [],
+      })
+      expect(response).to.be.deep.equal(payload);
+    });
+  });
+});
