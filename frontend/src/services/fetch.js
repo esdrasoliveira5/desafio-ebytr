d7279af3d5724757b097c65392d18dd39f7f0e19
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-19 22:10:48 -0300

    feat: implemented fetch.js in services

---
+++ frontend/src/services/fetch.js
@@ -0,0 +1,48 @@
+const URL_FETCH = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001/';
+const APLICATION = 'application/json';
+
+async function register({ userName, email, password }) {
+  try {
+    const response = await fetch(`${URL_FETCH}user`, {
+      method: 'POST',
+      headers: {
+        Accept: APLICATION,
+        'Content-Type': APLICATION,
+      },
+      body: JSON.stringify({
+        userName,
+        email,
+        password,
+      }),
+    });
+    const results = await response.json();
+    return results;
+  } catch (error) {
+    return { error };
+  }
+}
+
+async function loginUser({ email, password }) {
+  try {
+    const response = await fetch(`${URL_FETCH}user/login`, {
+      method: 'POST',
+      headers: {
+        Accept: APLICATION,
+        'Content-Type': APLICATION,
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const results = await response.json();
+    return results;
+  } catch (error) {
+    return { error };
+  }
+}
+
+export default {
+  register,
+  loginUser,
+};
