640789fae0f040070f0022152153a68d0c179b26
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-19 21:25:46 -0300

    feat: implemented Routes in App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={< LoginPage />} />
+      <Route path="/home" element={< HomePage />} />
+    </Routes>
+  );
+}
+
+export default App;
