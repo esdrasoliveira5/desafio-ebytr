5fbfc9e51ecfeb7fb3b851cca8c1ccd106d60e37
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-20 01:47:42 -0300

    feat: implemented userLoginValidation in Zod

--- backend/src/validations/Zod.ts
+++ backend/src/validations/Zod.ts
@@ -1,10 +1,25 @@
 import { ResponseError } from '../interfaces/ResponsesInterfaces';
 import { UserData, UserDataSchema } from '../types/UserDataType';
+import { UserLogin, UserLoginSchema } from '../types/UserLoginType';
 
 class Zod {
+  protected userLogin = UserLoginSchema;
+
   protected userData = UserDataSchema;
 
+  userLoginValidation(obj: UserLogin): void | ResponseError {
+    const parsedUser = this.userLogin.safeParse(obj);
+    if (!parsedUser.success) {      
+      return {
+        status: 400,
+        response: { error: parsedUser.error },
+      };
+    }
+  }
+
   userDataValidation(obj: UserData): void | ResponseError {
+    const valid = this.userLoginValidation(obj);
+    if (valid) return valid;
     const parsedUser = this.userData.safeParse(obj);
     if (!parsedUser.success) {      
       return {
