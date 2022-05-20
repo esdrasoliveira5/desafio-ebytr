5a8c1a7b09b7612087c98540c0eb9cdced61035e
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-20 02:04:07 -0300

    feat: implemented Formlogin.jsx

--- frontend/src/Components/FormLogin.jsx
+++ frontend/src/Components/FormLogin.jsx
@@ -3,14 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import LabeledInput from './Input';
 import services from '../services/fetch';
 
-function FormRegister() {
+function FormLogin() {
   const navigate = useNavigate();
 
   const [form, setForm] = useState({
-    userName: '',
     email: '',
     password: '',
-    confirmPassword: '',
   });
 
   const handleForms = ({ target }) => {
@@ -21,9 +19,9 @@ function FormRegister() {
     });
   };
 
-  const handleRegister = async (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-    const response = await services.register(form);
+    const response = await services.loginUser(form);
     if (response.error === undefined) {
       localStorage.setItem('to-do', JSON.stringify(response));
       navigate('/home');
@@ -32,12 +30,6 @@ function FormRegister() {
 
   return (
     <form>
-      <LabeledInput
-        type="text"
-        name="userName"
-        value={form.userName}
-        handle={handleForms}
-      />
       <LabeledInput
         type="text"
         name="email"
@@ -50,20 +42,14 @@ function FormRegister() {
         value={form.password}
         handle={handleForms}
       />
-      <LabeledInput
-        type="password"
-        name="confirmPassword"
-        value={form.confirmPassword}
-        handle={handleForms}
-      />
       <button
         type="submit"
-        onClick={handleRegister}
+        onClick={handleLogin}
       >
-        Cadastrar
+        Login
       </button>
     </form>
   );
 }
 
-export default FormRegister;
+export default FormLogin;
